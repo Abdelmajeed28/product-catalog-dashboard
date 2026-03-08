c51df366d2c1d70a858b27dc2e551d5f5da1ac3f
Abdelmajeed Abdelfattah <megoo2810@gmail.com>
2026-03-09 01:51:34 +0200

    create product model and dbconection

---
+++ models/Product.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+    },
+    image: [{ type: String }],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Product =
+  mongoose.models.Product || mongoose.model("Product", productSchema);
