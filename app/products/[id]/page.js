12ee3f5184ba92324c990192847f607d541671f7
Abdelmajeed Abdelfattah <megoo2810@gmail.com>
2026-03-15 00:17:34 +0200

    add photo to product details

--- app/products/[id]/page.js
+++ app/products/[id]/page.js
@@ -1,19 +1,39 @@
 import connectDB from "@/lib/db";
 import Product from "@/models/Product";
+import Image from "next/image";
 
 export default async function ProductDetails({ params }) {
   await connectDB();
   const { id } = await params;
   const product = await Product.findById(id);
   return (
-    <div className="p-10">
-      <h1 className="text-3xl font-bold mb-4">{product.title}</h1>
+    // <div className="p-10">
+    //   <h1 className="text-3xl font-bold mb-4">{product.title}</h1>
 
-      <p className="text-gray-500 mb-4">{product.category}</p>
+    //   <p className="text-gray-500 mb-4">{product.category}</p>
 
-      <p className="mb-6">{product.description}</p>
+    //   <p className="mb-6">{product.description}</p>
 
-      <p className="text-xl font-bold">${product.price}</p>
+    //   <p className="text-xl font-bold">${product.price}</p>
+    // </div>
+    <div className="max-w-6xl mx-auto p-10 grid grid-cols-2 gap-10">
+      <div className="relative w-full h-[400px]">
+        <Image
+          src={product.images?.[0] || "/placeholder.jpg"}
+          alt={product.title}
+          fill
+          className="object-cover rounded-xl"
+        />
+      </div>
+      <div>
+        <h1 className="text-4xl font-bold mb-4">{product.title}</h1>
+
+        <p className="text-gray-500 mb-4">{product.category}</p>
+
+        <p className="text-lg mb-6">{product.description}</p>
+
+        <p className="text-2xl font-bold mb-6">${product.price}</p>
+      </div>
     </div>
   );
 }
